ce1b23f169cc8bd6f14e6077f0a1d95b31460dd5
pirogue.one <alyona.evg@yandex.ru>
2025-09-03 16:36:57 +0300

    added dist dir, semantic tags and swiper library

---
+++ js/swiper.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const heroSwiper = new Swiper("#hero-swiper", {
+    // autoplay: {
+    //     delay: 3000,
+    // },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    }
+  });
